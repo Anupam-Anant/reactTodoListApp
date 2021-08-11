d7b65e293da09e34e52c342460e4829fb5a19aaf
Anupam Anant <anupam.anant90@gmail.com>
2021-08-11 22:30:38 +0530

    Initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    // let footerStyle = {
+    //     position: "absolute",
+    //     width: "100%",
+    //     top: "110vh"
+    // }
+    return (
+        <footer className="bg-dark text-light py-3" /*style={footerStyle}*/>
+            <p className="text-center">
+            Copyright &copy; Anupam's Todos List
+            </p>
+        </footer>
+    )
+}
